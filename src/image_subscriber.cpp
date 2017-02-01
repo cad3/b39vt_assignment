dc5c351f9492910804be8c98c88608393414cec7
Jose Part <joselpart@users.noreply.github.com>
2017-02-01 11:43:30 +0000

    Added comment.

--- src/image_subscriber.cpp
+++ src/image_subscriber.cpp
@@ -64,6 +64,7 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "image_subscriber");
   ImageSubscriber ic;
   
+  // Dummy variables - Replace with your own
   cv::Mat a;
   cv::Mat b;
   
